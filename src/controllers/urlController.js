c8a7b3da4749d6cf7d8633a285553a7eb29b9cea
Phyllipe <phylipe.sjc@hotmail.com>
2023-09-26 16:31:18 -0300

    Criada a urlController

---
+++ src/controllers/urlController.js
@@ -0,0 +1,34 @@
+const Link = require('../models/linkModel');
+const urlModel = require('../models/linkModel');
+
+const generateCode = () => {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 5; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+}
+
+module.exports = {
+  async createShortUrl(req, res, next) {
+    try {
+      const url = req.body.url;
+      const code = generateCode();
+      const domain = process.env.DOMAIN;
+  
+      const result = await Link.create({
+        url,
+        code
+      })
+
+      const newUrl = { newUrl: domain + result.code };
+
+      res.status(201).send(newUrl);
+
+    } catch (error) {
+      res.status(400).send("Erro ao adicionar ao banco");
+    }
+  }
+}
