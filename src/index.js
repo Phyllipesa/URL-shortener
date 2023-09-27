0300be178e514ed8e529815f19dfbf025eb1be37
Phyllipe <phylipe.sjc@hotmail.com>
2023-09-27 17:35:38 -0300

    Implementação do errorHandler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sequelize = require('./config/database');
-const Routes = require("./routes/index.routes")
+const Routes = require("./routes/index.routes");
+const { errorHandler } = require('./middlewares/errorHandler');
 
 
 const app = express();
@@ -10,6 +11,8 @@ sequelize.sync().then(() => console.log("database connected sucessfully o/"));
 app.use(express.json());
 
 app.use("/", Routes);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Server started on port 3000 =)");
