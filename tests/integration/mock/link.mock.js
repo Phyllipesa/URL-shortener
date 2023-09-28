d012fc61c52cea7f195e3ee83564c5fb7f919dd8
Phyllipe <phylipe.sjc@hotmail.com>
2023-09-28 16:00:57 -0300

    Criado mock para os testes.

---
+++ tests/integration/mock/link.mock.js
@@ -0,0 +1,24 @@
+const code = "fxWEL"
+
+const mockNewUrl = {
+	newUrl: 'http://localhost:3000/fxWEL'
+};
+
+const mockToDB = {
+  url: "https://exemplo.exemplo/exemplo/exemplo/exemplo.html",
+  code: "fxWEL"
+}
+
+const mockUrlCreated = {
+	hits: 0,
+	id: 1,
+	url: "https://exemplo.exemplo/exemplo/exemplo/exemplo.html",
+	code: "fxWEL"
+};
+
+module.exports = {
+  code,
+  mockToDB,
+  mockNewUrl,
+  mockUrlCreated
+}
